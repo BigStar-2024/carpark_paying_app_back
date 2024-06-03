522c304b2a8ce8b765a55e16b20e7e3074d2d3f1
BigStar2024 <aadsf7463@gmail.com>
2024-06-03 19:46:18 +0900

    update

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-const mongoURI = "mongodb://127.0.0.1:27017/paymentLog";
+const mongoURI = "mongodb://ec2-16-170-230-29.eu-north-1.compute.amazonaws.com:27017/paymentLog";
 
 module.exports = {
     mongoURI,
