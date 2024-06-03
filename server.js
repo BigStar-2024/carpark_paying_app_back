8cebf52f252a4add04122606bec9c42afe670514
BigStar2024 <aadsf7463@gmail.com>
2024-06-03 11:34:53 +0900

    6-3 Big update(striped log, dashboard database integration)

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const cors = require('cors');
 
 const app = express();
- // Use CORS middleware
- app.use(cors());
+// Use CORS middleware
+app.use(cors());
 // This is your test secret API key.
 const stripe = require("stripe")("sk_test_51P2lpQC9Zd6I2Ms1GvOYwHZUIGfQFbny1XJBEdABFVaLlko3erp8Zk5brxb7dHQJj45Hl0kVb3ddFb56nRdEHfwK00XRDvuqaN");
 app.use(express.static("public"));
@@ -12,6 +12,11 @@ app.use(express.json());
 const userRouter = require('./routers/user');
 app.use("/", userRouter);
 
+
+// const paymentIntentss = await stripe.paymentIntents.list({
+//   limit: 3,
+// });
+// console.log(paymentIntentss)
 
 const calculateOrderAmount = (value) => {
   // Replace this constant with a calculation of the order's amount
@@ -26,6 +31,7 @@ const chargeCustomer = async (customerId) => {
     customer: customerId,
     type: "card",
   });
+  console.log(paymentMethods);
   try {
     // Charge the customer and payment method immediately
     const paymentIntent = await stripe.paymentIntents.create({
@@ -59,7 +65,7 @@ app.post("/create-payment-intent", async (req, res) => {
   const paymentIntent = await stripe.paymentIntents.create({
     customer: customer.id,
     setup_future_usage: "off_session",
-    amount: calculateOrderAmount(items[0].amount*100),
+    amount: calculateOrderAmount(items[0].amount * 100),
     currency: "usd",
     // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
     automatic_payment_methods: {
@@ -67,10 +73,23 @@ app.post("/create-payment-intent", async (req, res) => {
     },
   });
 
+
+
   res.send({
     clientSecret: paymentIntent.client_secret,
   });
 });
 
+app.get('/payments_log', async (req, res) => {
+  const paymentIntents_list = await stripe.paymentIntents.list({
+    limit: 1,
+  });
+  // console.log(paymentIntents_list);
+  console.log(paymentIntents_list.data);
+  res.send({data: paymentIntents_list.data});
+  
+})
 
+
+// Start the server
 app.listen(4242, () => console.log("Node server listening on port 4242!"));
