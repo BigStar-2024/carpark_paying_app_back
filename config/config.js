6cca43518520c5f6b109892bcf4cb1bd573f1978
BigStar2024 <aadsf7463@gmail.com>
2024-06-18 05:44:13 +0900

    mongoURI update

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
 const mongoURI = "mongodb+srv://bigstar2024skytech:354GRw1LueHPif9w@cluster0.pkllvrb.mongodb.net/city-park-lot?retryWrites=true&w=majority&appName=Cluster0";
+// const mongoURI = "mongodb://127.0.0.1:27017";
 
 module.exports = {
     mongoURI,
